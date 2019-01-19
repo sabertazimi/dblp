7ed4c1708fa0d13a83f7a6bbeae5a7f16198d033
sabertazimi <sabertazimi@gmail.com>
2019-01-19 13:24:54 +0800

    fix(logger): remove redux logger when production
    issue sabertazimi/awesome-notes#105

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,22 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import createRootReducer from '../reducers';
 
-const loggerMiddleware = createLogger();
+const middleware = [];
+
+middleware.push(thunkMiddleware);
+
+// apply logger middleware when development
+// looger middleware must be placed at last posision
+if (process.env.NODE_ENV === 'development') {
+  const loggerMiddleware = createLogger();
+  middleware.push(loggerMiddleware);
+}
 
 const configureStore = (preloadedState) => {
   const store = createStore(
     createRootReducer(),
     preloadedState,
-    compose(
-      applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware,
-      ),
-    ),
+    compose(applyMiddleware(...middleware)),
   );
 
   return store;
