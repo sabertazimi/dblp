fb30e4be2e46bd651307d4fe3689b7516389f1f8
sabertazimi <sabertazimi@gmail.com>
2021-07-23 00:07:36 +0800

    update(postcss): dump to postcss-loader v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      'browsers': [
-        '> 0.1%',
-        'last 10 versions'
-      ]
-    })
+    require('autoprefixer'),
   ]
 };
