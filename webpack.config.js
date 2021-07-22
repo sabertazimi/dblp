8e94fb9b530ab24eb63b30dfbd7c9903c366ddee
sabertazimi <sabertazimi@gmail.com>
2021-07-22 23:15:41 +0800

    update(deps): update deps to date

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
@@ -46,7 +46,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin('build'),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       hash: true,
       template: './src/index.html',
