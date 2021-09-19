6a6fab973807a770741c5f76d2c0823863775ef4
sabertazimi <sabertazimi@gmail.com>
2021-09-19 14:33:48 +0800

    build(webpack-eslint): remove deprecated `eslint-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
@@ -22,7 +23,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
+        use: ['babel-loader']
       },
       {
         test: /\.html$/,
@@ -57,6 +58,7 @@ module.exports = {
       filename: devMode ? '[name].css' : '[name].[contenthash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css'
     }),
+    new ESLintPlugin({ extensions: ['tsx', 'ts', 'jsx', 'js'] }),
     new StyleLintPlugin(),
     devMode &&
       new BundleAnalyzerPlugin({
