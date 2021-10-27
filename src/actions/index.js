d13c4cf0f189773a5d9316cf3f10ed6577e8ec6c
sabertazimi <sabertazimi@gmail.com>
2021-10-28 00:35:19 +0800

    feat(Result): add paper citations data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from '../constants';
-import { dblpQuery, normalize } from '../api';
+import { dblpQuery, scQueryForCitations, scQueryForID } from '../api';
 
 const makeActionCreator =
   (type, ...argNames) =>
@@ -24,35 +24,111 @@ const requestData = makeActionCreator(ActionTypes.REQUEST_DATA, 'query');
 const receiveData = makeActionCreator(ActionTypes.RECEIVE_DATA, 'items');
 const requestError = makeActionCreator(ActionTypes.REQUEST_ERROR, 'error');
 
-export const fetchData = (keyword, venues) => dispatch => {
+let KEY = 0;
+
+export const fetchData = (keyword, venues) => async dispatch => {
   dispatch(requestData(venues));
 
-  return Promise.all(
+  const paperResponse = await Promise.all(
     venues.map(venue =>
       fetch(dblpQuery(keyword, venue), {
         method: 'GET',
         mode: 'cors',
       })
     )
-  )
-    .then(responses =>
-      Promise.all(
-        responses.map(response => {
-          if (response.ok) {
-            return response.json();
-          }
+  );
 
-          return null;
-        })
-      )
-    )
-    .then(data => {
-      if (!data) {
-        dispatch(requestError(new Error('Bad Request')));
+  const paperJson = await Promise.all(
+    paperResponse.map(response => (response.ok ? response.json() : null))
+  );
+
+  if (!paperJson) {
+    dispatch(requestError(new Error('Bad Request')));
+  }
+
+  const paperData = paperJson
+    .map(({ result }) => {
+      const { hit } = result.hits;
+
+      if (!hit) {
+        return [];
       }
 
-      setTimeout(() => {
-        dispatch(receiveData(normalize(data)));
-      }, 255);
-    });
+      return hit.map(({ info }) => ({
+        key: KEY++,
+        title: info.title,
+        venue: info.venue,
+        year: info.year,
+        url: info.ee,
+      }));
+    })
+    .flat();
+
+  let scPaperCitationData = Array(paperData.length).fill(0);
+
+  try {
+    const scPaperIdResponse = await Promise.all(
+      paperData.map(paper =>
+        fetch(scQueryForID(paper.title), {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        })
+      )
+    );
+
+    const scPaperIdJson = await Promise.all(
+      scPaperIdResponse.map(response => (response.ok ? response.json() : {}))
+    );
+
+    if (!scPaperIdJson) {
+      dispatch(requestError(new Error('Bad Request')));
+    }
+
+    const scPaperIdData = scPaperIdJson.map(paper =>
+      paper && paper.data[0] ? paper.data[0].paperId : '0'
+    );
+
+    const scPaperCitationResponse = await Promise.all(
+      scPaperIdData.map(paperId =>
+        fetch(scQueryForCitations(paperId), {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        })
+      )
+    );
+
+    const scPaperCitationJson = await Promise.all(
+      scPaperCitationResponse.map(response =>
+        response.ok ? response.json() : {}
+      )
+    );
+
+    if (!scPaperCitationJson) {
+      dispatch(requestError(new Error('Bad Request')));
+    }
+
+    scPaperCitationData = scPaperCitationJson.map(paperCitation =>
+      paperCitation && paperCitation.data ? paperCitation.data.length : 0
+    );
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Up to API limits');
+  }
+
+  const paperDataWithCitations = paperData.map((paper, index) => ({
+    ...paper,
+    citations: scPaperCitationData[index],
+  }));
+
+  setTimeout(() => {
+    dispatch(receiveData(paperDataWithCitations));
+  }, 255);
 };
--- src/api/index.js
+++ src/api/index.js
@@ -1,208 +1,13 @@
 import venuesData from '../venues.json';
-//import 'regenerator-runtime/runtime';
 
 export const dblpQuery = (keyword, venue) =>
   `https://dblp.org/search/publ/api?q=${keyword} venue:${venue}:&format=json&h=999`;
 
-export const SCQueryForID = title =>
+export const scQueryForID = title =>
   `https://api.semanticscholar.org/graph/v1/paper/search?query=${title}&limit=1`;
 
-export const SCQueryForCitations = paperid =>
+export const scQueryForCitations = paperid =>
   `https://api.semanticscholar.org/graph/v1/paper/${paperid}/citations`;
-
-let KEY = 0;
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-/*const getCitation = async (info) => {
-  let citation = 0;
-  const headers = new Headers();
-
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-  // headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
-  headers.append('Origin', 'http://localhost:3000');
-
-  await fetch(SCQueryForID(info.title), {
-    method: 'GET',
-    mode: 'cors',
-    headers,
-  })
-    //   )
-    .then(response => {
-      console.log('get response');
-
-      if (response.ok) {
-        return response.json();
-      }
-      console.log(`response is not ok ${response.status}`);
-      console.log(response.header);
-      console.log(response.body);
-      return null;
-    })
-    .then(data => {
-      if (!data) {
-        return null;
-      } else {
-        return data.data[0].paperId;
-      }
-    })
-    .then(paperId => {
-      if (!paperId) {
-        // eslint-disable-next-line no-console
-        console.log('no paperID');
-      } else {
-        console.log('query about citation');
-        sleep(300);
-        // eslint-disable-next-line no-console
-
-        //    Promise.all(
-        // return (
-        fetch(SCQueryForCitations(paperId), {
-          method: 'GET',
-          mode: 'cors',
-          headers,
-        })
-          //  )
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            return null;
-          })
-          .then(data => {
-            if (!data) {
-              // eslint-disable-next-line no-console
-              console.log('no citations');
-              return 0;
-            } else {
-              return data.data.length;
-            }
-          })
-          .then(length => {
-            // eslint-disable-next-line no-console
-            console.log(`the citations is ${length}`);
-            citation = length;
-          });
-        //  );
-      }
-    });
-  return citation;
-}*/
-
-export const normalize = data =>
-  data
-    .map(({ result }) => {
-      const { hit } = result.hits;
-
-      if (!hit) {
-        return [];
-      }
-
-
-      return hit.map(({ info }) => {
-
-       // const citation = getCitation(info);
-        //   Promise.all(
-        let citation = 0;
-        const headers = new Headers();
-
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        // headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
-        headers.append('Origin', 'http://localhost:3000');
-
-        fetch(SCQueryForID(info.title), {
-          method: 'GET',
-          mode: 'cors',
-          headers,
-        })
-          //   )
-          .then(response => {
-            console.log('get response');
-
-            if (response.ok) {
-              return response.json();
-            }
-            console.log(`response is not ok ${response.status}`);
-            console.log(response.header);
-            console.log(response.body);
-            return null;
-          })
-          .then(data => {
-            if (!data) {
-              return null;
-            } else {
-              return data.data[0].paperId;
-            }
-          })
-          .then(paperId => {
-            if (!paperId) {
-              // eslint-disable-next-line no-console
-              console.log('no paperID');
-            } else {
-              console.log('query about citation');
-              sleep(300);
-              // eslint-disable-next-line no-console
-
-              //    Promise.all(
-              // return (
-              fetch(SCQueryForCitations(paperId), {
-                method: 'GET',
-                mode: 'cors',
-                headers,
-              })
-                //  )
-                .then(response => {
-                  if (response.ok) {
-                    return response.json();
-                  }
-                  return null;
-                })
-                .then(data => {
-                  if (!data) {
-                    // eslint-disable-next-line no-console
-                    console.log('no citations');
-                    return 0;
-                  } else {
-                    return data.data.length;
-                  }
-                })
-                .then(length => {
-                  // eslint-disable-next-line no-console
-                  console.log(`the citations is ${length}`);
-                  citation = length;
-                });
-              //  );
-            }
-          });
-
-        console.log(`the citation is ${citation}`);
-        return {
-          key: KEY++,
-          title: info.title,
-          venue: info.venue,
-          year: info.year,
-          url: info.ee,
-          citations: citation,
-        };
-        /*  {
-            key: KEY++,
-            title: info.title,
-            venue: info.venue,
-            year: info.year,
-            url: info.ee,
-            citations: 1,
-          } */
-      });
-    })
-    .flat();
 
 export const VenuesDB = venuesData;
 
@@ -214,9 +19,8 @@ export const DEFAULT_VENUES_LIST = VENUES_LIST.slice(0, 30);
 
 export const getVenueTitle = venue => getVenueItem(venue).title || venue;
 
-export const getFilteredData = (items, { venues, year }) => {
-  console.log(items);
-  return items
+export const getFilteredData = (items, { venues, year }) =>
+  items
     .filter(
       item => venues.includes(item.venue) && parseInt(item.year, 10) >= year
     )
@@ -224,7 +28,6 @@ export const getFilteredData = (items, { venues, year }) => {
       ...item,
       venue: getVenueTitle(item.venue),
     }));
-};
 
 export const getStatisticsData = (items, { venues, year }) => {
   const filteredItems = getFilteredData(items, { venues, year });
