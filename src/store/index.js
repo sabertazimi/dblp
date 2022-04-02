094340860d52171c241a8a8e262cbf2deda586fe
Sabertaz <sabertazimi@gmail.com>
2022-04-02 14:56:27 +0800

    fix(redux): drop `redux-logger` (#274)

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,9 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import createRootReducer from '../reducers';
 
 const middleware = [];
-
 middleware.push(thunkMiddleware);
-
-// apply logger middleware when development
-// looger middleware must be placed at last posision
-if (process.env.NODE_ENV === 'development') {
-  const loggerMiddleware = createLogger();
-  middleware.push(loggerMiddleware);
-}
 
 const configureStore = preloadedState => {
   const store = createStore(
