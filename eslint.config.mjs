2e17fd461f2f28edabc3c2abc1f26e4f10312963
Sabertaz <sabertazimi@gmail.com>
2025-12-20 01:25:04 +0800

    chore(deps): update `@dg-scripts` packages to v6.0.0 (#664)
    * chore(deps): update `@dg-scripts` packages to v6.0.0
    
    * chore(eslint-config): add name to rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
-import eslintConfig from '@dg-scripts/eslint-config'
+import { defineConfig } from '@dg-scripts/eslint-config'
 
-export default eslintConfig.append({
-  files: ['**/*.js?(x)'],
-  rules: {
-    'react/jsx-uses-vars': 'error',
-    'react/prop-types': 'off',
-    'security/detect-object-injection': 'off',
+export default defineConfig(
+  { typescript: true },
+  {
+    name: 'react',
+    files: ['**/*.js?(x)'],
+    rules: {
+      'react/jsx-uses-vars': 'error',
+      'react/prop-types': 'off',
+    },
   },
-})
+)
